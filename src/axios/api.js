465ef70df442ea23b096d4f542b15a5d51075f58
artemZimin <66185060+artemZimin@users.noreply.github.com>
2021-03-10 20:02:20 +0300

    change api url

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8888/api',
+    baseURL: 'https://vast-wildwood-72848.herokuapp.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
